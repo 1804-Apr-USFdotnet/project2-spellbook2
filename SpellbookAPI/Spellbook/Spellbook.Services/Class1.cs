48868b90d3ae52ff91dbd0290420e3e5c999a18a
ndrwhapgood <ndrwhapgood@gmail.com>
2018-05-17 14:52:31 -0400

    debug this
    made it as the starting project and changed properties to be console application for debugging

--- SpellbookAPI/Spellbook/Spellbook.Services/Class1.cs
+++ SpellbookAPI/Spellbook/Spellbook.Services/Class1.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Spellbook.Models;
+
+using Spellbook.DataContext;
+
 
 namespace Spellbook.Services
 {
-    public class Class1
-    {
-    }
+	public class Class1
+	{
+		spellbookDbContext _context = new spellbookDbContext();
+		public void ClassAsIntGen()
+		{
+			IEnumerable<Spell> x = _context.Spells;
+			
+			foreach(var i in x)
+			{
+				Console.WriteLine(i.);
+			}
+			Console.ReadLine();
+		}
+	}
+
+	public class start
+	{
+		public static void Main()
+		{
+			var x = new Class1();
+			x.ClassAsIntGen();
+		}
+	}
 }
