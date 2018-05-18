19f7ace06b926982b96c1652c73184c0d51e14e5
ndrwhapgood <ndrwhapgood@gmail.com>
2018-05-18 11:51:38 -0400

    model made

--- SpellbookAPI/Spellbook/Spellbook.Models/Spell.cs
+++ SpellbookAPI/Spellbook/Spellbook.Models/Spell.cs
@@ -4,42 +4,58 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Spellbook.Models
 {
-    public class Spell
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int SpellId { get; set; }
+	public class Spell
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int SpellId { get; set; }
 
-        public string Name { get; set; }
+		public string Name { get; set; }
 
-        public string School { get; set; }
+		public string School { get; set; }
 
-        public int Level { get; set; }
+		public int Level { get; set; }
 
-        public Boolean Ritual { get; set; }
+		public Boolean Ritual { get; set; }
 
-        public string CastingTime { get; set; }
+		public string CastingTime { get; set; }
 
-        public string Source { get; set; }
+		public string Source { get; set; }
 
-        public string Range { get; set; }
+		public string Range { get; set; }
 
-        public string Classes { get; set; }
+		public string Classes { get; set; }
 
-        public string Components { get; set; }
+		public string Components { get; set; }
 
-        public string Duration { get; set; }
+		public string Duration { get; set; }
 
-        public string AtHigherLevel { get; set; }
+		public string AtHigherLevel { get; set; }
 
-        public Boolean Concentration { get; set; }
+		public Boolean Concentration { get; set; }
 
-        public string Slug { get; set; }
+		public string Slug { get; set; }
 
-        public int? Page { get; set; }
+		public int? Page { get; set; }
 
-        public string Description { get; set; }
+		public string Description { get; set; }
 
-        public int? ClassesAsInt { get; set; }
-    }
+		public int? ClassesAsInt { get; set; }
+	}
+
+	public class User
+	{
+
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int UserId { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		public int? Phone { get; set; }
+
+		[Required]
+		public string Email { get; set; }
+	}
 }
