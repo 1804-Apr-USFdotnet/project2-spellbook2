d83212bd27d77ccd11265cf5696df347866fd240
Ernie <Ernie@ERNIE-LAPTOP>
2018-05-18 11:24:52 -0400

    Clean up
    deleting some comments and stuff

--- SpellbookAPI/Spellbook/Spellbook.Repositories/CharacterRepository.cs
+++ SpellbookAPI/Spellbook/Spellbook.Repositories/CharacterRepository.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Spellbook.DataContext;
 using Spellbook.Models;
 
 namespace Spellbook.Repositories
 {
    public class CharacterRepository: Repository<SpellbookDbContext, Character>
-   { }
-    
+   { }    
 }
