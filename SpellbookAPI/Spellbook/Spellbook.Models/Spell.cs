68cd7226ff415f0cadf585ffaae638c6654d84a7
Cameron Wagstaff <cam.wagstaff@gmail.com>
2018-05-18 11:45:28 -0400

    Adding spellbook model and mapping to spells
    Many to many relation between spellbook and spell is resolved in models.

--- SpellbookAPI/Spellbook/Spellbook.Models/Spell.cs
+++ SpellbookAPI/Spellbook/Spellbook.Models/Spell.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -41,5 +42,7 @@ namespace Spellbook.Models
         public string Description { get; set; }
 
         public int? ClassesAsInt { get; set; }
+
+        public virtual ICollection<SpellbookSpells> Spellbooks { get; set; }
     }
 }
