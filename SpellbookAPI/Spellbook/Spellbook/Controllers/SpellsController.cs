2bce5209a0df414a43b9dbd68db2e96a4aa0588c
James Grippo <jgrippo95@gmail.com>
2018-05-18 12:31:17 -0400

    Created Spells controller.

---
+++ SpellbookAPI/Spellbook/Spellbook/Controllers/SpellsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Spellbook.Models;
+using Spellbook.Services;
+
+namespace Spellbook.Controllers
+{
+    public class SpellsController : ApiController
+    {
+        private readonly SpellbookService _service = new SpellbookService();
+        // GET api/values
+        public IEnumerable<Spell> Get()
+        {
+            return _service.GetAllSpells();
+        }
+
+        // GET api/values/5
+        public Spell Get(int id)
+        {
+            return _service.GetSpellBy(id);
+        }
+    }
+}
