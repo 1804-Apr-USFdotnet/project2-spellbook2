e9e7db1e10f3a1f3d338a13e7d4bd1cdd2d7a496
James Grippo <jgrippo95@gmail.com>
2018-05-19 19:13:35 -0400

    Added get by Schools and Levels.

--- SpellbookAPI/Spellbook/Spellbook.Services/QueryStringService.cs
+++ SpellbookAPI/Spellbook/Spellbook.Services/QueryStringService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,34 +12,51 @@ namespace Spellbook.Services
 {
     class QueryStringService
     {
-        public string queryString { get; set; }
+        private string _queryString { get; set; }
+
 
         public QueryStringService(string qs)
         {
-            queryString = qs;
+            _queryString = qs;
         }
 
-        public Expression<Func<Spell, bool>> StringHashSet(string filter)
+        public void StringPredicate(string filter, out Expression<Func<Spell, bool>> predicate)
         {
             string[] querySplit;
             switch (filter)
             {
                 case "Classes":
                 {
-                    querySplit = queryString.Split(',');
-
+                    querySplit = _queryString.Split(',');
+                    predicate = (x => querySplit.Contains(x.Classes));
                     break;
                 }
                 case "Schools":
                 {
-                    querySplit = queryString.Split(',');
+                    querySplit = _queryString.Split(',');
+                    predicate = (x => querySplit.Contains(x.School));
                     break;
                 }
                 default:
                 {
+                    querySplit = _queryString.Split(',');
+                    predicate = (x => querySplit.Contains(x.School));
                     break;
                 }
             }
-            return
+        }
+        public void intPredicate(string filter, out Expression<Func<Spell, bool>> predicate)
+        {
+            string[] querySplit = _queryString.Split(',');
+            HashSet<int> levels = new HashSet<int>();
+
+            foreach (string s in querySplit)
+            {
+                levels.Add(Int32.Parse(s));
+            }
+
+            predicate = (x => levels.Contains(x.Level));
+
+        }
     }
 }
