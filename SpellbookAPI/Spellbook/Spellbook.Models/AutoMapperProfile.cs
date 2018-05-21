54b289d4ced973b85daabf2c20ba0fe1df995f80
James Grippo <jgrippo95@gmail.com>
2018-05-21 11:06:02 -0400

    Created AutoMapper profile.

---
+++ SpellbookAPI/Spellbook/Spellbook.Models/AutoMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Spellbook.Models
+{
+    class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Spell, SpellDTO>();
+        }
+    }
+}
