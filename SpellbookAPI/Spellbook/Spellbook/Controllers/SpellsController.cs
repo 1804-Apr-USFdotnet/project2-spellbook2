b5c91eb2968d427a2d237da9398c488ab55bee94
James Grippo <jgrippo95@gmail.com>
2018-05-22 14:46:38 -0400

    IT WORKS

--- SpellbookAPI/Spellbook/Spellbook/Controllers/SpellsController.cs
+++ SpellbookAPI/Spellbook/Spellbook/Controllers/SpellsController.cs
@@ -11,37 +11,37 @@ namespace Spellbook.Controllers
     {
         private readonly SpellbookService _service = new SpellbookService();
  
-        public IHttpActionResult Get()
-        {
-            var req = Request.GetQueryNameValuePairs();
-            if(req.Count() != 0)
-                return BadRequest("Invalid Parameters! :(");
-            return Ok(_service.GetAllSpells());
-        }
+        //public IHttpActionResult Get()
+        //{
+        //    var req = Request.GetQueryNameValuePairs();
+        //    if(req.Count() != 0)
+        //        return BadRequest("Invalid Parameters! :(");
+        //    return Ok(_service.GetAllSpells());
+        //}
 
         public IHttpActionResult Get(int id)
         {
             return Ok(_service.GetSpellBy(id));
         }
 
-        public IHttpActionResult Get(string queryString, string filter)
-        {
-            string[] availableFilters = { "levels", "classes", "schools" };
-            try
-            {
-                if (!availableFilters.Contains(filter))
-                {
-                    return BadRequest("Your filter was inccorect! :(");
-                }
+        //public IHttpActionResult Get(string queryString, string filter)
+        //{
+        //    string[] availableFilters = { "levels", "classes", "schools" };
+        //    try
+        //    {
+        //        if (!availableFilters.Contains(filter))
+        //        {
+        //            return BadRequest("Your filter was inccorect! :(");
+        //        }
 
-                return Ok(_service.GetSpellBy(queryString, filter));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
+        //        return Ok(_service.GetSpellBy(queryString, filter));
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //        throw;
+        //    }
+        //}
 
         public IHttpActionResult Get([FromUri] SpellQuery query)
         {
