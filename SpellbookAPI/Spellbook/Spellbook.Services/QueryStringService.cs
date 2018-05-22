a925b7530f67208471ed8c39fac5d2a584fc6031
James Grippo <jgrippo95@gmail.com>
2018-05-22 14:33:18 -0400

    First try compound search

--- SpellbookAPI/Spellbook/Spellbook.Services/QueryStringService.cs
+++ SpellbookAPI/Spellbook/Spellbook.Services/QueryStringService.cs
@@ -8,12 +8,18 @@ namespace Spellbook.Services
 {
     class QueryStringService
     {
-        private string _queryString { get; set; }
+        private string _queryString { get; }
+        private SpellQuery _spellQuery { get; }
 
 
         public QueryStringService(string qs)
         {
             _queryString = qs;
+        }
+
+        public QueryStringService(SpellQuery sq)
+        {
+            _spellQuery = sq;
         }
 
         public void StringPredicate(string filter, out Expression<Func<Spell, bool>> predicate)
@@ -53,6 +59,21 @@ namespace Spellbook.Services
 
             predicate = (x => levels.Contains(x.Level));
 
+        }
+
+        public void CompoundQuery(out Expression<Func<Spell, bool>> predicate)
+        {
+            string[] s_levels = _spellQuery.levels.Split(',');
+            string[] school = _spellQuery.school.Split(',');
+            int classes = new BitwiseService().ClassConverter(_spellQuery.classes);
+
+            HashSet<int> levels = new HashSet<int>();
+            foreach (string s in s_levels)
+            {
+                levels.Add(Int32.Parse(s));
+            }
+
+            predicate = (x=> school.Contains(x.School) && levels.Contains(x.Level) && ((classes & x.ClassesAsInt) == classes) );
         }
     }
 }
