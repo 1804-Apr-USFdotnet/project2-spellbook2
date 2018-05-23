89fb0f44c1605a4bbfbc58aa6cd697e6d09811e4
James Grippo <jgrippo95@gmail.com>
2018-05-23 12:18:50 -0400

    Partial class for services.

---
+++ SpellbookAPI/Spellbook/Spellbook.Services/SpellListService.cs
@@ -0,0 +1,23 @@
+﻿using Spellbook.Models;
+using Spellbook.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using AutoMapper;
+
+namespace Spellbook.Services
+{
+    public partial class SpellbookService
+    {
+        private readonly SpellListRepository _spellLists = new SpellListRepository();
+        private readonly SpellListSpellsRepository _spellListsSpells = new SpellListSpellsRepository();
+
+        public SpellList GetSpellListBy(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
