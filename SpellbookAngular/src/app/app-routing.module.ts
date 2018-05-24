be0f3f2726617f021c2b53428b976d2dd01b5e6c
James Grippo <jgrippo95@gmail.com>
2018-05-24 16:10:39 -0400

    Routing for spells.

--- SpellbookAngular/src/app/app-routing.module.ts
+++ SpellbookAngular/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { SpellComponentComponent } from './Components/spell-component/spell-component.component';
+import { SpellComponent } from './Components/spell/spell.component';
 
 const appRoutes: Routes = [
-  {path: "spells", component: SpellComponentComponent}
+  {path: "spells", component: SpellComponent}
 ]
 
 @NgModule({
--- SpellbookAngular/src/app/app.module.ts
+++ SpellbookAngular/src/app/app.module.ts
@@ -7,14 +7,12 @@ import { AppRoutingModule } from './/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
-import { SpellComponentComponent } from './Components/spell-component/spell-component.component';
-import { SpellComponent } from './spell/spell.component';
+import { SpellComponent } from './Components/spell/spell.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    SpellComponentComponent,
     SpellComponent
   ],
   imports: [
