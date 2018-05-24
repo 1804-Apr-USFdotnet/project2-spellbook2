cc2eb551c81240cba0e471319f5695d6a71155da
James Grippo <jgrippo95@gmail.com>
2018-05-24 14:21:55 -0400

    Angular navigate & routing

--- SpellbookAngular/SpellbookAngular/src/app/app.module.ts
+++ SpellbookAngular/SpellbookAngular/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from './/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
