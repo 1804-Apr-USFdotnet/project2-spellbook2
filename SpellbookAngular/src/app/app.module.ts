01b0a4e37f312727c935e32839ecc66bbb50f00a
James Grippo <jgrippo95@gmail.com>
2018-05-25 10:45:17 -0400

    Bootstrap.

--- SpellbookAngular/src/app/app.module.ts
+++ SpellbookAngular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './/app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
@@ -16,6 +17,7 @@ import { SpellComponent } from './Components/spell/spell.component';
     SpellComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     RouterModule,
