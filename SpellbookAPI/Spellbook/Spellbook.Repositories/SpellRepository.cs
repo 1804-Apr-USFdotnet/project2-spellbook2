9b401cacbb59972fd61fce364805685ffc15d29e
James Grippo <jgrippo95@gmail.com>
2018-05-25 10:54:10 -0400

    Individual repos.

--- SpellbookAPI/Spellbook/Spellbook.Repositories/SpellRepository.cs
+++ SpellbookAPI/Spellbook/Spellbook.Repositories/SpellRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -9,7 +10,46 @@ using Spellbook.Models;
 
 namespace Spellbook.Repositories 
 {
-    public class SpellRepository : ARepository<SpellbookDbContext, Spell>
+    public class SpellRepository : IRepository<Spell>
     {
+        private readonly IDBContext _context;
+
+        public SpellRepository(IDBContext c)
+        {
+            _context = c;
+        }
+
+        public IEnumerable<Spell> GetAll()
+        {
+            IEnumerable<Spell> query = _context.Set<Spell>();
+            return query;
+        }
+
+        public IEnumerable<Spell> FindBy(Expression<Func<Spell, bool>> predicate)
+        {
+
+            IEnumerable<Spell> query = _context.Set<Spell>().Where(predicate);
+            return query;
+        }
+
+        public virtual void Add(Spell entity)
+        {
+            _context.Set<Spell>().Add(entity);
+        }
+
+        public virtual void Delete(Spell entity)
+        {
+            _context.Set<Spell>().Remove(entity);
+        }
+
+        public virtual void Edit(Spell entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Save()
+        {
+            _context.SaveChanges();
+        }
     }
 }
