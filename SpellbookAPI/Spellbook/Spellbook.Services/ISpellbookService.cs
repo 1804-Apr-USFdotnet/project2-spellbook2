0f6a67eb86d2f0316104800eb9c4d6552e405f0f
James Grippo <jgrippo95@gmail.com>
2018-05-25 09:42:17 -0400

    Interface for service

---
+++ SpellbookAPI/Spellbook/Spellbook.Services/ISpellbookService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Spellbook.Models;
+
+namespace Spellbook.Services
+{
+    public interface ISpellbookService
+    {
+        List<Spell> GetAllSpells();
+
+        SpellDTO GetSpellBy(int id);
+
+        List<SpellDTO> GetSpellBy(SpellQuery query);
+
+        SpellListDTO GetSpellListBy(int id);
+
+        List<SpellListDTO> GetAllSpellList();
+
+        void AddSpellList(SpellListDTO spellList);
+
+        void EditSpellList(SpellListDTO newSpellList);
+
+        void DeleteSpellList(int id);
+    }
+}
